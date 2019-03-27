f69598936867914a35a30b06d30281cecfb8cca9
Smarthard <th3smartchan@gmail.com>
2019-03-27 22:17:05 +0500

    loaded repositories caching

--- src/app/services/GithubService.ts
+++ src/app/services/GithubService.ts
@@ -11,32 +11,36 @@ export class GithubService {
 
     public getMyRepos(): Promise<GitRepository[]> {
         return new Promise<GitRepository[]>((resolve, reject) => {
-            this.http.get('https://api.github.com/users/Smarthard/repos').subscribe(
-                (res) => {
-                    for (let obj in res) {
-                        let repo: GitRepository = new GitRepository(res[obj]);
+            if (this.repositories.length == 0) {
+                this.http.get('https://api.github.com/users/Smarthard/repos').subscribe(
+                    (res) => {
+                        for (let obj in res) {
+                            let repo: GitRepository = new GitRepository(res[obj]);
 
-                        if (repo.fork) {
-                            this.getRepoInfo(repo.url).subscribe(
-                                res => {
-                                    repo = new GitRepository(res);
-                                    this.repositories.push(repo);
-                                },
-                                err => {
-                                    console.error(err);
-                                })
-                        } else {
-                            this.repositories.push(repo);
+                            if (repo.fork) {
+                                this.getRepoInfo(repo.url).subscribe(
+                                    res => {
+                                        repo = new GitRepository(res);
+                                        this.repositories.push(repo);
+                                    },
+                                    err => {
+                                        console.error(err);
+                                    })
+                            } else {
+                                this.repositories.push(repo);
+                            }
                         }
-                    }
 
-                    resolve(this.repositories);
-                },
-                (err) => {
-                    console.error(err);
-                    reject(err);
-                }
-            )
+                        resolve(this.repositories);
+                    },
+                    (err) => {
+                        console.error(err);
+                        reject(err);
+                    }
+                )
+            } else {
+                resolve(this.repositories);
+            }
         });
     }
 
