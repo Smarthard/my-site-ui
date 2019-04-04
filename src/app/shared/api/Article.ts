c41b3957363be790888ffe890263bdc02d4d0006
Smarthard <th3smartchan@gmail.com>
2019-04-04 21:02:01 +0300

    Article text from DB parse method

--- src/app/shared/api/Article.ts
+++ src/app/shared/api/Article.ts
@@ -4,10 +4,15 @@ export class Article {
     public text: string;
     readonly createdAt: Date;
 
+    private static parseTextFromBD(text: string): string {
+        return text
+                .split("\n").join("<BR>");
+    }
+
     constructor(json) {
         this.id = json.id;
         this.name = json.name;
-        this.text = json.text;
+        this.text = Article.parseTextFromBD(json.text);
         this.createdAt = new Date(Date.parse(json.createdAt));
     }
 
